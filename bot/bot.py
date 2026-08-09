14c5114a54f49b147a20495b0adcd1f462a144ff
Borikmm <106659966+Borikmm@users.noreply.github.com>
2026-08-09 14:14:58 +1000

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,14 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from .config import TOKEN
 from .user_service import UserService
 from .book_service import BookService
 from .quote_service import QuoteService
 from .stats_service import StatsService
 from .scheduler import Scheduler
-from .handlers import Handlers, TITLE, AUTHOR, TOTAL_PAGES, NORM
+from .handlers import Handlers
 import logging
+import asyncio
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,25 +25,18 @@ class LibraryBot:
 
     def _register_handlers(self):
         self.app.add_handler(CommandHandler("start", self.handlers.start_command))
+        self.app.add_handler(CommandHandler("cancel", self.handlers.cancel))
         self.app.add_handler(CallbackQueryHandler(self.handlers.button_callback))
-
-        
-        conv_handler = ConversationHandler(
-            entry_points=[CommandHandler("change_book", self.handlers.change_book_start)],
-            states={
-                TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.change_book_title)],
-                AUTHOR: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.change_book_author)],
-                TOTAL_PAGES: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.change_book_total)],
-                NORM: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.change_book_norm)],
-            },
-            fallbacks=[CommandHandler("cancel", self.handlers.cancel)]
-        )
-        self.app.add_handler(conv_handler)
-        
-        # Обработчик текстовых сообщений (только для отметки)
         self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.handle_text))
 
+    async def run_async(self):
+        """Асинхронный запуск бота"""
+        self.scheduler.start()
+        await self.app.initialize()
+        await self.app.start()
+        await self.app.updater.start_polling()
+        await self.app.updater.idle()
 
     def run(self):
-        self.scheduler.start()
-        self.app.run_polling(allowed_updates=Update.ALL_TYPES)
+        """Синхронный запуск (для локального тестирования)"""
+        asyncio.run(self.run_async())
